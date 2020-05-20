890c5d6f281250c762ffcfaea624d5d58c2d8808
Ghost <13hackghost@gmail.com>
2020-05-20 08:51:31 -0400

    Updated server to use the new config file

--- server.js
+++ server.js
@@ -2,16 +2,16 @@
 const express = require('express');
 const app = express();
 
-if (process.env.NODE_ENV === "development" || typeof(process.env.NODE_ENV) === "undefined" ){
+if (process.env.NODE_ENV !== "production"){
 	
 	var webpack = require('webpack');
 	var webpackDevMiddleware = require('webpack-dev-middleware');
 	var webpackHotMiddleware = require('webpack-hot-middleware');
 	
-	var webpackConfig = require('./webpack.config.js');
+	var webpackConfig = require('./webpack.dev.js');
 	var compiler = webpack(webpackConfig);
 	
-	// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+	// Tell express to use the webpack-dev-middleware and use the webpack.dev.js
 	// configuration file as a base.
 	app.use(webpackDevMiddleware(compiler, {
 		publicPath: webpackConfig.output.publicPath,
