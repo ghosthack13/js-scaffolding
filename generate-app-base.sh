5e97fe0c132a0662ae6363667f3c32b9aeb051be
Ghost <13hackghost@gmail.com>
2020-05-28 16:45:09 -0400

    build: Improve initial directory structure and add likely needed modules

--- generate-app-base.sh
+++ generate-app-base.sh
@@ -4,24 +4,27 @@
 npm init -y
 
 #Create directory structure
-# mkdir public src dist
+# mkdir -p config dist src/{components, models, pages, routes, servies}
 # touch ./src/index.js
 
+npm install mysql2
 npm install express
 npm install --save-dev nodemon
+npm install --save-dev eslint
 
 
 # --------------------------- Install Webpack and Plugins/Loaders ---------------------
-#Install Webpack Tools
+#Install Core Webpack Tools
 npm install --save-dev webpack webpack-cli
 npm install --save-dev webpack-dev-server
 npm install --save-dev webpack-dev-middleware webpack-hot-middleware
-npm install --save-dev webpack-merge
+npm install --save-dev webpack-merge	#Used to merge configs
 
-#Install Webpack Plugins and Loaders
+#Install Webpack Plugins
 npm install --save-dev html-webpack-plugin  # Simplifies creation of HTML files to serve your bundles
 npm install --save-dev clean-webpack-plugin	# Remove files not used by project
 
+#Install Webpack Loaders
 npm install --save-dev css-loader	#parses the CSS into JavaScript and resolves any dependencies
 npm install --save-dev style-loader	#outputs our CSS into a <style> tag in the HTML document
 
@@ -34,7 +37,8 @@ npm install --save-dev babel-loader
 #Install Bable and common Presets
 npm install --save-dev @babel/core @babel/cli @babel/preset-env
 npm install --save-dev @babel/plugin-proposal-class-properties
-npm install --save-dev @babel/polyfill core-js
+npm install --save core-js
+# npm install @babel/polyfill #-- Deprecated as of Babel 7.4.0 inn favor of core-js
 
 
 # --------------------------- Install React ---------------------
@@ -42,5 +46,8 @@ npm install --save-dev @babel/polyfill core-js
 npm install react react-dom
 npm install react-router-dom
 npm install --save-dev react-hot-loader
+npm install --save-dev eslint-plugin-react
+npm install --save-dev eslint-plugin-react-hooks
 npm install --save-dev @babel/preset-react
+npm install --save-dev @babel/plugin-transform-react-jsx-source #Shows line numbers in error boundaries, Disable in Production
 
