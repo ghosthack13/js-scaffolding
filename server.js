8412da63ee39880afde63365c5eedfcef74b2373
Ghost <13hackghost@gmail.com>
2020-05-28 17:08:13 -0400

    refactor: add comments, refactor express server and writing dev server files to disk

--- server.js
+++ server.js
@@ -1,27 +1,51 @@
+/**//**************************************************************************
+ *
+ *	@author Jone Doe <jdoe@example.com>
+ *	@file server.js
+ *
+ *	This is the main entry point
+ *
+ ******************************************************************************/
+
+"use strict"
+
+//!Create Server Resources
+const http = require("http");
 
 const express = require('express');
 const app = express();
 
 if (process.env.NODE_ENV !== "production"){
-	
+
 	var webpack = require('webpack');
+
 	var webpackDevMiddleware = require('webpack-dev-middleware');
 	var webpackHotMiddleware = require('webpack-hot-middleware');
-	
+
 	var webpackConfig = require('./webpack.dev.js');
 	var compiler = webpack(webpackConfig);
-	
+
 	// Tell express to use the webpack-dev-middleware and use the webpack.dev.js
 	// configuration file as a base.
 	app.use(webpackDevMiddleware(compiler, {
 		publicPath: webpackConfig.output.publicPath,
+		writeToDisk: true,
 	}));
-	
+
 	app.use(webpackHotMiddleware(compiler));
 
 }
 
-// Serve the files on port 3000.
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!\n');
+//! Set ports to listen on (unprivileged ports >= 1024)
+const HTTP_PORT = 3000;
+
+
+//! ------------------- Import Routing Handlers/EndPoints -------------------
+
+
+//! Load HTTP Server
+let httpServer = http.createServer(app);
+httpServer.listen(HTTP_PORT, function(){
+	console.log(`Fitness Tracker App`);
+	console.log(`Listening on port ${HTTP_PORT}!`);
 });
