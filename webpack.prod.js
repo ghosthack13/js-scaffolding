c6e692263e4d60e6e6f847a82d60b7181438af04
Ghost <13hackghost@gmail.com>
2020-05-29 11:27:09 -0400

    refactor: Add production output/optimization settings

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,26 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+	
 	mode: 'production',
+	
+	output: {
+		filename: '[name].[contenthash].js',
+		chunkFilename: '[name].[contenthash].js',
+	},
+	
+	optimization: {
+		moduleIds: 'hashed',
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+			},
+		},
+	},
+	
 });
