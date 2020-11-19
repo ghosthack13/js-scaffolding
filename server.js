7b36e8c8e3076ebeb6ec31db8a9bc3ed3093e82b
Ghost <13hackghost@gmail.com>
2020-11-18 22:38:36 -0400

    refactor: Change var to const and make source more generic

--- server.js
+++ server.js
@@ -1,51 +1,52 @@
 /**//**************************************************************************
  *
- *	@author Jone Doe <jdoe@example.com>
+ *	@author <author> <author@example.com>
  *	@file server.js
  *
  *	This is the main entry point
  *
  ******************************************************************************/
 
-"use strict"
+"use strict";
+
+const path = require("path");
 
 //!Create Server Resources
 const http = require("http");
 
-const express = require('express');
+const express = require("express");
 const app = express();
+
+//! Set ports to listen on (unprivileged ports >= 1024)
+const HTTP_PORT = process.env.NODE_HTTP_PORT || 3000;
 
 if (process.env.NODE_ENV !== "production"){
 
-	var webpack = require('webpack');
+	const webpack = require("webpack");
 
-	var webpackDevMiddleware = require('webpack-dev-middleware');
-	var webpackHotMiddleware = require('webpack-hot-middleware');
+	const webpackDevMiddleware = require("webpack-dev-middleware");
+	const webpackHotMiddleware = require("webpack-hot-middleware");
 
-	var webpackConfig = require('./webpack.dev.js');
-	var compiler = webpack(webpackConfig);
+	const webpackConfig = require("./webpack.dev.js");
+	const compiler = webpack(webpackConfig);
 
 	// Tell express to use the webpack-dev-middleware and use the webpack.dev.js
 	// configuration file as a base.
 	app.use(webpackDevMiddleware(compiler, {
+		index: false, // Do not respond to request to the root URL (Express will handle it)
 		publicPath: webpackConfig.output.publicPath,
 		writeToDisk: true,
 	}));
 
 	app.use(webpackHotMiddleware(compiler));
-
 }
 
-//! Set ports to listen on (unprivileged ports >= 1024)
-const HTTP_PORT = 3000;
-
-
 //! ------------------- Import Routing Handlers/EndPoints -------------------
-
+app.use(express.static(path.join(__dirname, "dist"), {index: false}));
 
 //! Load HTTP Server
 let httpServer = http.createServer(app);
 httpServer.listen(HTTP_PORT, function(){
-	console.log(`Fitness Tracker App`);
+	console.log("Server Activated");
 	console.log(`Listening on port ${HTTP_PORT}!`);
 });
